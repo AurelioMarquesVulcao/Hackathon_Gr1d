71b446041b0103a969b2b3af95a104d8015c731d
Aurelio Marques Vulcão <avaliacoesvulcao@gmail.com>
2020-04-18 19:28:45 -0300

    atualizações

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [],
+  bootstrap:[AppComponent]
 })
 export class AppRoutingModule { }
