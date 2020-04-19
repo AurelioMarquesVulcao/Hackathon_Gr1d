cc0ec3a03d08f5ce83fc3cfc68854a91558b56e2
Aurelio Marques Vulcão <avaliacoesvulcao@gmail.com>
2020-04-19 16:31:22 -0300

    testes e tentarivas de correção no projeto

---
+++ my-app/src/components/cnh/cnh.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Axios from 'axios';
+import Header from '../header/header'
+
+
+// content type json aplication  -  header - e um body
+
+// const URL = Axios.post('https://gateway.gr1d.io/sandbox/infocar/cnh/v1/INFOCAR_CNH', { headers: {'X-Api-Key': 'f1f8dd77-9fd9-4485-ae05-feca758ae9fb'}})
+//         .then(response => {
+//             console.log(response)
+//         })
+//         .catch(error => {
+//             console.log(error)
+//         })
+
+function Cnh_request() {
+    return (
+      <div className="App">
+        <Header />
+        cnh
+
+      </div>
+    );
+  }
+
+  console.log(URL)
+  
+  export default Cnh_request;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Cnh_request from './components/cnh/cnh';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar'
+
+
 
 ReactDOM.render(
   <React.StrictMode>
